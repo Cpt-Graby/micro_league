7b453b42f104750a7ec203b148d643f9db281a9b
Alexis <grablortooken@gmail.com>
2026-01-10 19:55:11 +0100

    FIX: a working raycasting, probably not the best system but it's working

--- src/players/player_base.gd
+++ src/players/player_base.gd
@@ -6,10 +6,13 @@ var target = Vector3.ZERO
 
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var camera_3D: Camera3D = $"../Camera3D"
+@onready var ray_query = PhysicsRayQueryParameters3D.new()
+
+
+var marker = preload("res://scene/marker.tscn")
 
 func _ready():
-	print("Hell0")
-	
+	pass
 
 func _physics_process(delta):
 	var next_path_point = $NavigationAgent3D.get_next_path_position()
@@ -19,10 +22,14 @@ func _physics_process(delta):
 	move_and_slide()
 
 func _process(delta: float) -> void:
-	#$camera3D.global_position = $camera_marker.global_position
-	#$camera3D.look_at(Vector3.ZERO,Vector3.UP)
+	camera_3D.global_position = $camera_marker.global_position
+	camera_3D.look_at(transform.origin,Vector3.UP)
 	pass
 
+func create_instance(given_position: Vector3) -> void:
+	var instance = marker.instantiate()
+	instance.position = given_position
+	$"..".add_child(instance)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -36,12 +43,30 @@ func _input(event: InputEvent) -> void:
 		print("random_position: ", random_pos)
 		navigation_agent.set_target_position(random_pos)
 
+
 func move_to_click(event: InputEvent):
-	var from = camera_3D.project_ray_origin(event.position)
-	var to = camera_3D.project_local_ray_normal(event.position)
-	var Cursor_pos = Plane(Vector3.UP, transform.origin.y).intersects_ray(from, to)
-	print("from: ",from)
-	print("to: ", to)
-	print("Cursor: ",Cursor_pos)
+	var ray_length = 10000
+	var space = get_world_3d().direct_space_state
+	var mouse_pos = get_viewport().get_mouse_position()
+	var from_mouse = camera_3D.project_ray_origin(mouse_pos)
+	var to = from_mouse + camera_3D.project_ray_normal(mouse_pos) * ray_length
+	
+	ray_query.from = from_mouse
+	ray_query.to = to
+	var raycast_result = space.intersect_ray(ray_query)
+	navigation_agent.set_target_position(raycast_result.position)
+	print("raycast res: ", raycast_result)
+
+	#var from = camera_3D.project_ray_origin(event.position)
+	#var dir = camera_3D.project_local_ray_normal(event.position) 
+	#var dir2 = camera_3D.project_local_ray_normal(event.position) * ray_length
+	#var Cursor_pos = Plane(Vector3.UP, transform.origin.y).intersects_ray(from, dir)
+	#var Cursor_pos2 = Plane(Vector3.UP, transform.origin.y).intersects_ray(from, dir2)
+	#print("from: ", from, "-", from_mouse, " :from_mouse")
+
+	#print("from: ",from)
+	#print("to: ", dir)
+	#print("dir: ", dir, "--- Cursor: ",Cursor_pos)
+	#print("dir: ", dir2, "--- Cursor2: ",Cursor_pos2)
 	#$NavigationAgent3D.target_position = Cursor_pos
 	pass
