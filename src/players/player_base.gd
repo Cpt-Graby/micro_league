6abdfc5be9bb9433da97fff3c472735b91739089
alexis <alexis@42lausanne.ch>
2026-01-10 17:37:06 +0100

    exploring the camera system

--- src/players/player_base.gd
+++ src/players/player_base.gd
@@ -5,45 +5,43 @@ const JUMP_VELOCITY = 4.5
 var target = Vector3.ZERO
 
 @onready var navigation_agent: NavigationAgent3D = $NavigationAgent3D
+@onready var camera_3D: Camera3D = $"../Camera3D"
 
 func _ready():
-	# These values need to be adjusted for the actor's speed
-	# and the navigation layout.
-	navigation_agent.path_desired_distance = 0.5
-	navigation_agent.target_desired_distance = 0.5
-	# Make sure to not await during _ready.
-	actor_setup.call_deferred()
-
-func actor_setup() -> void:
-	await get_tree().physics_frame
-	pass
-
-func set_movement_target(movement_target: Vector3)-> void:
-	navigation_agent.set_target_position(movement_target)
-	pass
-
+	print("Hell0")
+	
 
 func _physics_process(delta):
 	var next_path_point = $NavigationAgent3D.get_next_path_position()
 	var new_velocity = (next_path_point - global_position).normalized() * SPEED
 	velocity.x = new_velocity.x
-	velocity.y = new_velocity.y
+	velocity.z = new_velocity.z
 	move_and_slide()
 
-
 func _process(delta: float) -> void:
-	$camera3D.global_position = $camera_marker.global_position
-	$camera3D.look_at(Vector3.ZERO,Vector3.UP)
-	
+	#$camera3D.global_position = $camera_marker.global_position
+	#$camera3D.look_at(Vector3.ZERO,Vector3.UP)
+	pass
+
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if Input.is_action_just_pressed("move_click"):
 			move_to_click(event)
 			
+	if event.is_action_pressed("ui_accept"):
+		var random_pos := Vector3.ZERO
+		random_pos.x = randf_range(-2.5, 2.5)
+		random_pos.z = randf_range(-2.5, 2.5)
+		print("random_position: ", random_pos)
+		navigation_agent.set_target_position(random_pos)
+
 func move_to_click(event: InputEvent):
-	var from = $camera3D.project_ray_origin(event.position)
-	var to = from + $camera3D.project_local_ray_normal(event.position) * 1000
+	var from = camera_3D.project_ray_origin(event.position)
+	var to = camera_3D.project_local_ray_normal(event.position)
 	var Cursor_pos = Plane(Vector3.UP, transform.origin.y).intersects_ray(from, to)
-	print(Cursor_pos)
-	$NavigationAgent3D.target_position = Cursor_pos
+	print("from: ",from)
+	print("to: ", to)
+	print("Cursor: ",Cursor_pos)
+	#$NavigationAgent3D.target_position = Cursor_pos
 	pass
