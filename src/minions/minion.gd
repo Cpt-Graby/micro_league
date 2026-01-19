ffe3be39cd3fde2b9f8f35ccf61992640e218e98
Alexis <grablortooken@gmail.com>
2026-01-19 20:05:06 +0100

    ADD: of some animation for the minions

--- src/minions/minion.gd
+++ src/minions/minion.gd
@@ -25,7 +25,7 @@ var g_team := ""  # "blue" or "red"
 var g_target_spawn := Vector3.ZERO
 var g_health :  int = 465
 var g_max_health: int = 465
-var g_attack_range : float = 1.500  
+var g_attack_range : float = 2.500  
 var g_attack_speed : float = 1.25
 var g_attack_dmg : int = 21
 var g_armor :float = 0
@@ -34,10 +34,10 @@ var g_time_last_attack = 0
 
 
 func _ready() -> void:
-	health_bar.max_value = g_max_health
-	health_bar.value = g_health
 	my_node = get_node(".")
 	add_to_group("minions")
+	health_bar.max_value = g_max_health
+	health_bar.value = g_health
 	navigation_agent.velocity_computed.connect(Callable(_on_velocity_computed))
 	set_movement_target(g_target_spawn)
 	
@@ -72,11 +72,42 @@ func get_index_closest_enemy(enemy_distance_list : Array)-> int:
 			index_closest = i
 	return index_closest
 
+
+func _acquisition_target() -> void:
+	var all_minions = get_tree().get_nodes_in_group("minions")
+	var minions_in_range_dist : Array
+	var minions_in_range : Array
+	for minion in all_minions:
+		var dist_to_minion = dist_in_ranged_self(minion.position)
+		if (minion != my_node and minion.g_team != my_node.g_team \
+		and dist_to_minion <= g_detection_range):
+			minions_in_range_dist.append(dist_to_minion)
+			minions_in_range.append(minion)
+	# Now we want to determine the if we have to set a target
+	if len(minions_in_range) > 0:
+		g_target_enemy = minions_in_range[get_index_closest_enemy(minions_in_range_dist)]
+		g_target_locked = true
+
+
+func _attack():
+	if !g_target_enemy:
+		return
+	g_target_enemy.take_damage(g_attack_dmg)
+
+
+func take_damage(amount : int):
+	g_health -= amount
+	health_bar.value = g_health
+	if g_health <= 0:
+		queue_free()
+
+
 func _process(delta):
 	g_time_last_attack += delta
 	if g_target_enemy and g_target_locked == true:
 		if dist_in_ranged_self(g_target_enemy.position) <= g_attack_range:
 			set_movement_target(global_position)
+			navigation_agent.target_reached
 			if g_time_last_attack > g_attack_speed:
 				_attack()
 				g_time_last_attack = 0
@@ -86,7 +117,8 @@ func _process(delta):
 		g_target_locked = false
 		if navigation_agent.target_position != g_target_spawn:
 			set_movement_target(g_target_spawn)
-
+	if g_target_locked == false:
+		_acquisition_target()
 
 
 func _physics_process(delta):
@@ -103,22 +135,6 @@ func _physics_process(delta):
 		await get_tree().create_timer(1.0).timeout
 		queue_free()
 		return
-	
-	# Determining it we need to attack minion
-	var all_minions = get_tree().get_nodes_in_group("minions")
-	var minions_in_range_dist : Array
-	var minions_in_range : Array
-	for minion in all_minions:
-		var dist_to_minion = dist_in_ranged_self(minion.position)
-		if (minion != my_node and minion.g_team != my_node.g_team \
-		and dist_to_minion <= g_detection_range):
-			minions_in_range_dist.append(dist_to_minion)
-			minions_in_range.append(minion)
-	# Now we want to determine the if we have to set a target
-	if len(minions_in_range) > 0 and g_target_locked == false:
-		g_target_enemy = minions_in_range[get_index_closest_enemy(minions_in_range_dist)]
-		g_target_locked = true
-	
 	var next_path_position: Vector3 = navigation_agent.get_next_path_position()
 	var new_velocity: Vector3 = global_position.direction_to(next_path_position) * movement_speed
 	if navigation_agent.avoidance_enabled:
@@ -126,14 +142,3 @@ func _physics_process(delta):
 		#print("new_velocity set")
 	else:
 		_on_velocity_computed(new_velocity)
-
-func _attack():
-	if !g_target_enemy:
-		return
-	g_target_enemy.take_damage(g_attack_dmg)
-
-func take_damage(amount : int):
-	g_health -= amount
-	health_bar.value = g_health
-	if g_health <= 0:
-		queue_free()
